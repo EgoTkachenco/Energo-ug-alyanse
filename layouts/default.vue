12004919d8d6dda11dbe91d919fc3d1380aba899
EgoTkachenco <egortkachenco@gmail.com>
2021-01-27 16:46:46 +0200

    Repository restructure

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="wrapper">
+    <Navigation></Navigation>
+    <div class="content">
+      <Nuxt />
+    </div>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Navigation from '../components/navigation';
+import Footer from '../components/footer';
+export default {
+  components: {
+    Navigation, Footer
+  }
+}
+</script>
+
+<style lang="scss">
+.content {
+  flex: 1 1;
+}
+</style>
