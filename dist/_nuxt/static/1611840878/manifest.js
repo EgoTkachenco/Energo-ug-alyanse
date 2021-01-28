3e15b6c0608f4c54d782a38bba525b2363e8ce9f
EgoTkachenco <egortkachenco@gmail.com>
2021-01-28 16:14:56 +0200

    fix

--- dist/_nuxt/static/1611840878/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fru\u002Fabout-business-point","\u002Fportfolio","\u002Fcontacts","\u002Fru\u002Fcontacts","\u002Fru\u002Fportfolio","\u002Fru","\u002Fabout-business-point"]})
