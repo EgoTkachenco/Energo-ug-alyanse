04f883bc1f70981908000f0daae448bbabfa866a
EgoTkachenco <egortkachenco@gmail.com>
2021-01-28 16:10:27 +0200

    change base route

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,6 @@
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
-  router: {
-    base: 'https://egotkachenco.github.io/Energo-ug-alyanse'
-  },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'site-generator',
